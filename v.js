3551e57f00d5690d8ee8e940731090f197b008f0
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-09-10 12:26:37 +0800

    Update v.js

--- v.js
+++ v.js
@@ -1,5 +1,5 @@
 document.write(`
 
-<div style="text-align: center;color: #FFB6C1;font-size: 10px;">2023 © v1.2</div>
+<div style="text-align: center;color: #FFB6C1;font-size: 10px;">2023 © v1.1</div>
 
 `);
