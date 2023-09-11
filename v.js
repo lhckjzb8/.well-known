3894d320317b55a2318814919f6061a5ee4631e8
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-09-11 09:57:06 +0800

    Update v.js

--- v.js
+++ v.js
@@ -1,5 +1,25 @@
 document.write(`
 
-<div style="text-align: center;color: #FFB6C1;font-size: 10px;">2023 © v1.1</div>
+<style>
+.div{text-align: center;padding: 10px;}
+.v{color: #FFB6C1;font-size: 10px;}
+.width{width: 100%;}
+.jx{text-align: left;width: 40%;}
+.jx span{color: #A58888;}
+.jx label{color: #A15888;}
+.left{float: left;}
+.right{float: right;}
+</style>
+<div class="div v width left">2023 © v1.1</div>
+<div class="div left width"><div style="padding: 0 10%;">
+<div class="jx left"><label>103-107期</label>〖兔蛇鸡羊〗(今年未错)</div>
+<div class="jx right"><label>100-104期</label>〖05.15.25.35.45〗</div>
+</div>
+</div>
+<div class="div left width"><div style="padding: 0 10%;">
+<div class="jx left"><label>103-107期</label>〖41,42,43,44,45,46〗</div>
+<div class="jx right"><label>103-111期</label>〖11,22,33,44〗<span>(今年错一)<span></div>
+</div>
+</div>
 
 `);
