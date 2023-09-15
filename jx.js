143a30c625f2aa3ba33e5d8984c8e79b45c2b2c1
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-09-15 16:45:43 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -1,16 +1,16 @@
 ﻿function jxhtml(){
-jxzl.innerHTML='<div class="jx">
-  <p><label>105-110期 </label>17,18,19,20,21,22,23<span> (今年错1)</span><span style="color:#ccc;font-size: 8px;">错061—070期</span></p><!--0尾期L3左右3-->
-  <p><label>105-106期 </label>鸡羊蛇兔猪牛<span> (今年未错)</span></p>
-  <p><label>105-107期 </label>牛猪鸡鼠<span> (今年未错)</span></p>
-  <p><label>105-109期 </label>15,16,17,18,19,35,37</p>
-  <p><label>103-107期 </label>41,42,43,44,45,46</p>
-  <p><label>101-107期 </label>02,05,20,24,27,32<span> (连准11轮)</span></p><!--金鳞,刹破狼-->
-  <p><label>101-110期 </label>牛鼠猪<span> (连准9轮,今年无错)</span></p><!--0尾期L2减1.2-->
-  <p><label>101-110期开码表 </label>02,03,04,12,13,15,18,20,39,40,44,46,（12码）<span> (至少开2期)</span></p>
-  <p style="padding:8px;border-bottom: 3px solid #1E90FF;"></p>
-  <p><label>103-107期 </label>兔<font color=red>蛇</font>鸡羊 <span>(今年未错，104期开蛇)</span></p>
-  <p><label>103-105期 </label>鼠马龙狗鸡<font color=red>蛇</font><span> (今年错1，104期开蛇)</span></p>
-  <p><label>103-111期 </label><font color=red>11</font>,22,33,44<span> (今年错1，104期开11)</span></p>
+jxzl.innerHTML='<div class="jx">\
+  <p><label>105-110期 </label>17,18,19,20,21,22,23<span> (今年错1)</span><span style="color:#ccc;font-size: 8px;">错061—070期</span></p><!--0尾期L3左右3-->\
+  <p><label>105-106期 </label>鸡羊蛇兔猪牛<span> (今年未错)</span></p>\
+  <p><label>105-107期 </label>牛猪鸡鼠<span> (今年未错)</span></p>\
+  <p><label>105-109期 </label>15,16,17,18,19,35,37</p>\
+  <p><label>103-107期 </label>41,42,43,44,45,46</p>\
+  <p><label>101-107期 </label>02,05,20,24,27,32<span> (连准11轮)</span></p><!--金鳞,刹破狼-->\
+  <p><label>101-110期 </label>牛鼠猪<span> (连准9轮,今年无错)</span></p><!--0尾期L2减1.2-->\
+  <p><label>101-110期开码表 </label>02,03,04,12,13,15,18,20,39,40,44,46,（12码）<span> (至少开2期)</span></p>\
+  <p style="padding:8px;border-bottom: 3px solid #1E90FF;"></p>\
+  <p><label>103-107期 </label>兔<font color=red>蛇</font>鸡羊 <span>(今年未错，104期开蛇)</span></p>\
+  <p><label>103-105期 </label>鼠马龙狗鸡<font color=red>蛇</font><span> (今年错1，104期开蛇)</span></p>\
+  <p><label>103-111期 </label><font color=red>11</font>,22,33,44<span> (今年错1，104期开11)</span></p>\
   </div>';
 };
