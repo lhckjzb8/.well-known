39517422ea90769b1ded061361e5f3f363790cc0
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-09-15 16:51:16 +0800

    Update gs.js

--- gs.js
+++ gs.js
@@ -1,4 +1,23 @@
 
 function gshtml(){
-gsgl.innerHTML='<div class="img"><img src="https://lx36.mh40.com/tp/guilv1.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv2.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv3.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv4.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv5.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv6.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv7.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv8.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv9.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv10.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv11.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv12.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv13.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv14.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv15.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv16.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv17.jpg?'+Math.random()+'" width="100%"></div><div class="img"><img src="https://lx36.mh40.com/tp/guilv18.jpg?'+Math.random()+'" width="100%"></div>';
+gsgl.innerHTML='\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv1.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv2.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv3.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv4.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv5.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv6.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv7.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv8.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv9.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv10.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv11.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv12.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv13.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv14.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv15.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv16.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv17.jpg?'+Math.random()+'" width="100%"></div>\
+  <div class="img"><img src="https://lx36.mh40.com/tp/guilv18.jpg?'+Math.random()+'" width="100%"></div>\
+  ';
 };
