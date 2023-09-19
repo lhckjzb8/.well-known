93dc1e10b9e642f7e0e3da0f7b50ec2091adc40a
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-09-19 22:06:26 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -1,3 +1,21 @@
 ﻿function jxhtml(){
-jxzl.innerHTML='<div class="jx">\</div>';
- }
+jxzl.innerHTML='<div class="jx">\
+  <p><label>109期平特 </label>04,07<span> (今年未错)</span></p>\
+  <p><label>106-112期 </label>02,09,16,23,30,37,44<span> (今年错1)</span></p>\
+  <p><label>106-108期 </label>牛羊龙狗<span> (今年错1)</span></p>\
+  <p><label>106-107期 </label>鸡猴羊牛龙蛇<span> (今年错1)</span></p>\
+  <p><label>105-108期 </label>01,10,18,22,26,28,31<span> (2中2)21组</span></p>\
+  <p><label>105-110期 </label>17,18,19,20,21,22,23<span> (今年错1)</span></p><!--0尾期L3左右3(错061-070)-->\
+  <p><label>105-107期 </label>牛猪鸡鼠<span> (今年未错)</span></p>\
+  <p><label>101-107期 </label>02,05,20,24,27,32<span> (连准11轮)</span></p><!--金鳞,刹破狼-->\
+  <p><label>101-110期 </label>牛鼠猪<span> (连准9轮,今年无错)</span></p><!--0尾期L2减1.2-->\
+  <p><label>101-110期开码表 </label>02,03,04,12,13,15,18,20,39,40,44,<font color=red>46</font>,（12码）<span> (至少开2期,106期46)</span></p>\
+  <p style="padding:8px;border-bottom: 1.5px solid #7B68EE;"></p>\
+  <p><label>103-107期 </label>41,42,43,44,45,<font color=red>46</font><span> (106期46)</span></p>\
+  <p><label>105-106期 </label>鸡羊<font color=red>蛇</font>兔猪牛<span> (今年未错，105期蛇)</span></p>\
+  <p><label>105-109期 </label>15,16,17,18,19,<font color=red>35</font>,37 <span>(105期35)</span></p>\
+  <p><label>103-107期 </label>兔<font color=red>蛇</font>鸡羊 <span>(今年未错，104期蛇)</span></p>\
+  <p><label>103-105期 </label>鼠马龙狗鸡<font color=red>蛇</font><span> (今年错1，104期蛇)</span></p>\
+  <p><label>103-111期 </label><font color=red>11</font>,22,33,44<span> (今年错1，104期11)</span></p>\
+  </div>';
+};
