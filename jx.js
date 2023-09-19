999c84f0f7f426f6667bf7625f5725f0c7d0ef63
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-09-19 20:35:57 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -1,24 +1,8 @@
 ﻿function jxhtml(){
 jxzl.innerHTML='<div class="jx">\</div>';
 //刷新
-document.writeln("<div class=\'refresh\' onclick=\'javascript:location.reload();\'>刷新</div>");
-document.writeln("<style>");
-document.writeln(".refresh {");
-document.writeln("    width: 40px;");
-document.writeln("    height: 40px;");
-document.writeln("    line-height: 40px;");
-document.writeln("    text-align: center;");
-document.writeln("    color: #fff;");
-document.writeln("    font-size: 14px;");
-document.writeln("    border-radius: 40px;");
-document.writeln("    box-sizing: unset;");
-document.writeln("    border: solid 4px rgba(255,255,255,.8);");
-document.writeln("    background: rgba(255,0,0,8);");
-document.writeln("    position: fixed;");
-document.writeln("    right: 10px;");
-document.writeln("    bottom: 20px;");
-document.writeln("    z-index: 1;");
-document.writeln("    cursor: pointer;");
-document.writeln("}");
-document.writeln("</style>");
+var newDiv = document.createElement("div");// 设置div的属性，例如id、class、style等
+newDiv.setAttribute("id", "mysx");
+document.body.appendChild(newDiv);  
+mysx.innerHTML = "<div style='width: 40px;height: 40px;line-height: 40px;text-align: center;color: #fff;font-size: 14px;border-radius: 40px;box-sizing: unset;border: solid 4px rgba(255,255,255,.8);background: rgba(255,0,0,8);position: fixed;right: 10px;bottom: 20px;z-index: 1;cursor: pointer;' onclick='javascript:location.reload();'>刷新</div>"
  }
