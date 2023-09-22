c281ca7dcf44f79b39f409f80fb21af87e3d5f75
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-09-22 22:54:13 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -3,6 +3,7 @@ document.write(`
   <p><label style="background-color:#fff211;color:#f00;">108-112期复试二三 06,09,16,18,21,34<span> (重点🤑)</span></label></p>
   <p style="padding:8px;border-bottom: 1.5px solid #7B68EE;"></p>
   <p><label>109期平特 </label>04,07<span> (今年未错)</span></p>
+  <p><label>108-109期 </label>02,22,12,24,20,32,05,29,25,49</p>
   <p><label>107-111期特尾 </label>8尾,9尾<span> (今年错1)</span></p>
   <p><label>107-116期 </label>21,33,27,39<span> (至少1次)</span></p>
   <p><label>107-108期 </label>18,19,24</p>
