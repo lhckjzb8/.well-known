26e45d9a63ca479ff553a3cbfb048a1bec9de528
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-09-25 23:22:10 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -4,8 +4,10 @@ document.write(`
   <p><label>109期 </label>234头<span> (连准18期)</span></p>
   <p><label style="background-color:#fff211;color:#f00;">108-112期复试二三 06,09,16,18,21,34<span> (重点🤑)</span></label></p>
   <p style="padding:8px;border-bottom: 1.5px solid #7B68EE;"></p>
-  <p><label>109期平特 </label>04,07<span> (今年未错)</span></p>
-  <p><label>108-109期 </label>绿波<span> (今年未错)</span></p>
+  <p><label>109-116期 </label>11,22,33,44,<span> (主22)</span></p>
+  <p><label>109-113期 </label>15,20,25,39,45,48,<span> (今年无错)</span></p>
+  <p><label>109期平特 </label>04,07<span> (今年无错)</span></p>
+  <p><label>108-109期 </label>绿波<span> (今年无错)</span></p>
   <p><label>107-111期特尾 </label>8尾,9尾<span> (今年错1)</span></p>
   <p><label>107-116期 </label>21,33,27,39<span> (至少1次)</span></p>
   <p><label>105-110期 </label>17,18,19,20,21,22,23<span> (今年错1)</span></p><!--0尾期L3左右3(错061-070)-->
@@ -18,9 +20,9 @@ document.write(`
   <p><label>106-112期 </label>02,09,16,23,30,<font color=red>37</font>,44<span> (今年错1，107期37)</span></p>
   <p><label>105-108期 </label><font color=red>01</font>,<font color=red>10</font>,18,<font color=red>22</font>,26,<font color=red>28</font>,31<span> (2中2)21组,107期01,10,22,28,😘</span></p>
   <p><label>103-107期 </label>41,42,43,44,45,<font color=red>46</font><span> (106期46)</span></p>
-  <p><label>105-106期 </label>鸡羊<font color=red>蛇</font>兔猪牛<span> (今年未错，105期蛇)</span></p>
+  <p><label>105-106期 </label>鸡羊<font color=red>蛇</font>兔猪牛<span> (今年无错，105期蛇)</span></p>
   <p><label>105-109期 </label>15,16,17,18,19,<font color=red>35</font>,37 <span>(105期35)</span></p>
-  <p><label>103-107期 </label>兔<font color=red>蛇</font>鸡羊 <span>(今年未错，104期蛇)</span></p>
+  <p><label>103-107期 </label>兔<font color=red>蛇</font>鸡羊 <span>(今年无错，104期蛇)</span></p>
   <p><label>103-105期 </label>鼠马龙狗鸡<font color=red>蛇</font><span> (今年错1，104期蛇)</span></p>
   <p><label>103-111期 </label><font color=red>11</font>,22,33,44<span> (今年错1，104期11)</span></p>
   </div>
