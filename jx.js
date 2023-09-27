66933ddb5aa87e36dc7379fc7cbd2b28f3b97caa
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-09-27 18:51:12 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -4,13 +4,15 @@ document.write(`
   <p><label>109期 </label>234头<span> (连准18期)</span></p>
   <p><label style="background-color:#fff211;color:#f00;">108-112期复试二三 06,09,16,18,21,34<span> (重点🤑)</span></label></p>
   <p style="padding:8px;border-bottom: 1.5px solid #7B68EE;"></p>
-  <p><label>109-116期 </label>11,22,33,44,<span> (主22)</span></p>
-  <p><label>109-113期 </label>15,20,25,39,45,48,<span> (今年无错)</span></p>
+  <p><label>109-116期 </label>11,22,33,44<span> (主22)</span></p>
+  <p><label>109-113期 </label>43,44,45,46,47,48</p>
+  <p><label>109-113期 </label>15,20,25,39,45,48<span> (今年无错)</span></p>
+  <p><label>109-111期 </label>蛇鸡马鼠<span> (今年错1)</span></p>
   <p><label>109期平特 </label>04,07<span> (今年无错)</span></p>
   <p><label>108-109期 </label>绿波<span> (今年无错)</span></p>
   <p><label>107-111期特尾 </label>8尾,9尾<span> (今年错1)</span></p>
   <p><label>107-116期 </label>21,33,27,39<span> (至少1次)</span></p>
-  <p><label>105-110期 </label>17,18,19,20,21,22,23<span> (今年错1)</span></p><!--0尾期L3左右3(错061-070)-->
+  <p><label>105-110期 </label>17,18,19,20,21,22,23<span> (今年错1)剩2</span></p><!--0尾期L3左右3(错061-070)-->
   <p><label>101-110期 </label>牛鼠猪<span> (连准9轮,今年无错)</span></p><!--0尾期L2减1.2-->
   <p><label>101-110期开码表 </label>02,03,04,<font color=red>12</font>,13,15,18,20,39,40,44,<font color=red>46</font>,（12码）<span> (至少开2期，106期46，108期12)</span></p>
   <p style="padding:8px;border-bottom: 1.5px solid #7B68EE;"></p>
