73f5d3cb5dd015dc05bae45abd8a096f59ce429f
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-10-06 02:34:56 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -65,13 +65,12 @@ line-height:23px;
   <p><label>107-116期 </label>21,33,27,39<span> (至少1次)</span></p>
   <p><label>111-120期开码表 </label>03,10,12,15,18,20,27,30,32,35,39,44,46,（13码）<span> (至少开2期)</span></p>
   <p style="padding:8px;border-bottom: 1.5px solid #7B68EE;"></p>
-  <p><label>109-113期 </label>15,20,25,39,<font color=red>45</font>,48<span> (今年无错)</span></p>
-  <p><label>111-115期 </label>虎牛猴<font color=red>羊</font><span> (两年错1)</span> + 兔鸡<span> (三年无错)</span></p><!--0,5尾期特加1,2两年错1（错在2023年10-15，多加0从2021年10期开始无错-->
+  <p><label>109-113期 </label>15,20,25,39,<font color=red>45</font>,48<span> (今年无错，112期45)</span></p>
+  <p><label>111-115期 </label>虎牛猴<font color=red>羊</font><span> (两年错1)</span> + 兔鸡<span> (三年无错，112期羊)</span></p><!--0,5尾期特加1,2两年错1（错在2023年10-15，多加0从2021年10期开始无错-->
   <p><label>107-111期特尾 </label>8尾,<font color=red>9尾</font><span> (今年错1，111期29)</span></p>
   <p><label>110-111期 </label><font color=red>兔</font>虎鼠猪鸡猴<span> (今年无错，110期兔)</span></p>
   <p><label>101-110期开码表 </label>02,03,04,<font color=red>12</font>,13,15,18,20,39,40,44,<font color=red>46</font>,（12码）<span> (至少开2期，106期46，108期12，109期46)</span></p>
-  <p><label>109-113期 </label>43,44,45,<font color=red>46</font>,47,48<span> (今年无
-错，109期46)</span></p>
+  <p><label>109-113期 </label>43,44,45,<font color=red>46</font>,47,48<span> (今年无错，109期46)</span></p>
   <p><label>109-111期 </label>蛇鸡<font color=red>马</font>鼠<span> (今年错1，109期马)</span></p>
   <p><label>108-109期 </label>02,22,<font color=red>12</font>,24,20,32,05,29,25,49<span> (108期12)</span></p>
   <p><label>106-108期 </label>牛羊<font color=red>龙</font>狗<span> (今年错1，108期龙)</span></p>
