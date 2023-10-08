b65cc7bc4446491bd9df4757d228e24fe7994ef1
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-10-08 14:56:56 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -1,5 +1,8 @@
 document.write(`
 <style>
+.jx font{
+color: #f00;
+}
 .gs{
 float: left;
 border-radius: 1px;
@@ -101,25 +104,25 @@ font-family: Helvetica, Arial, sans-serif;
   <p><label>107-116期 </label>21,33,27,39<span> (至少1次)</span></p>
   <p><label>111-120期开码表 </label>03,10,12,15,18,20,27,30,32,35,39,44,46,（13码）<span> (至少开2期)</span></p>
   <p style="padding:8px;border-bottom: 1.5px solid #7B68EE;"></p>
-  <p><label>112-113期 </label>兔<font color=red>虎</font>牛鼠猪狗鸡<span> (今年错2，113期虎)</span></p>
-  <p><label>109-113期 </label>15,20,25,39,<font color=red>45</font>,48<span> (今年无错，112期45)</span></p>
-  <p><label>111-115期 </label>虎牛猴<font color=red>羊</font><span> (两年错1)</span> + 兔鸡<span> (三年无错，112期羊)</span></p><!--0,5尾期特加1,2两年错1（错在2023年10-15，多加0从2021年10期开始无错-->
-  <p><label>107-111期特尾 </label>8尾,<font color=red>9尾</font><span> (今年错1，111期29)</span></p>
-  <p><label>110-111期 </label><font color=red>兔</font>虎鼠猪鸡猴<span> (今年无错，110期兔)</span></p>
-  <p><label>101-110期开码表 </label>02,03,04,<font color=red>12</font>,13,15,18,20,39,40,44,<font color=red>46</font>,（12码）<span> (至少开2期，106期46，108期12，109期46)</span></p>
-  <p><label>109-113期 </label>43,44,45,<font color=red>46</font>,47,48<span> (今年无错，109期46)</span></p>
-  <p><label>109-111期 </label>蛇鸡<font color=red>马</font>鼠<span> (今年错1，109期马)</span></p>
-  <p><label>108-109期 </label>02,22,<font color=red>12</font>,24,20,32,05,29,25,49<span> (108期12)</span></p>
-  <p><label>106-108期 </label>牛羊<font color=red>龙</font>狗<span> (今年错1，108期龙)</span></p>
-  <p><label>103-107期 </label><font color=red>兔</font>蛇鸡羊<span> (今年错1，107期兔)</span></p>
-  <p><label>106-112期 </label>02,09,16,23,30,<font color=red>37</font>,44<span> (今年错1，107期37)</span></p>
-  <p><label>105-108期 </label><font color=red>01</font>,<font color=red>10</font>,18,<font color=red>22</font>,26,<font color=red>28</font>,31<span> (2中2)21组,107期01,10,22,28,😘</span></p>
-  <p><label>103-107期 </label>41,42,43,44,45,<font color=red>46</font><span> (106期46)</span></p>
-  <p><label>105-106期 </label>鸡羊<font color=red>蛇</font>兔猪牛<span> (今年无错，105期蛇)</span></p>
-  <p><label>105-109期 </label>15,16,17,18,19,<font color=red>35</font>,37 <span>(105期35)</span></p>
-  <p><label>103-107期 </label>兔<font color=red>蛇</font>鸡羊 <span>(今年无错，104期蛇)</span></p>
-  <p><label>103-105期 </label>鼠马龙狗鸡<font color=red>蛇</font><span> (今年错1，104期蛇)</span></p>
-  <p><label>103-111期 </label><font color=red>11</font>,22,33,44<span> (今年错1，104期11)</span></p>
+  <p><label>112-113期 </label>兔<font>虎</font>牛鼠猪狗鸡<span> (今年错2，113期虎)</span></p>
+  <p><label>109-113期 </label>15,20,25,39,<font>45</font>,48<span> (今年无错，112期45)</span></p>
+  <p><label>111-115期 </label>虎牛猴<font>羊</font><span> (两年错1)</span> + 兔鸡<span> (三年无错，112期羊)</span></p><!--0,5尾期特加1,2两年错1（错在2023年10-15，多加0从2021年10期开始无错-->
+  <p><label>107-111期特尾 </label>8尾,<font>9尾</font><span> (今年错1，111期29)</span></p>
+  <p><label>110-111期 </label><font>兔</font>虎鼠猪鸡猴<span> (今年无错，110期兔)</span></p>
+  <p><label>101-110期开码表 </label>02,03,04,<font>12</font>,13,15,18,20,39,40,44,<font>46</font>,（12码）<span> (至少开2期，106期46，108期12，109期46)</span></p>
+  <p><label>109-113期 </label>43,44,45,<font>46</font>,47,48<span> (今年无错，109期46)</span></p>
+  <p><label>109-111期 </label>蛇鸡<font>马</font>鼠<span> (今年错1，109期马)</span></p>
+  <p><label>108-109期 </label>02,22,<font>12</font>,24,20,32,05,29,25,49<span> (108期12)</span></p>
+  <p><label>106-108期 </label>牛羊<font>龙</font>狗<span> (今年错1，108期龙)</span></p>
+  <p><label>103-107期 </label><font>兔</font>蛇鸡羊<span> (今年错1，107期兔)</span></p>
+  <p><label>106-112期 </label>02,09,16,23,30,<font>37</font>,44<span> (今年错1，107期37)</span></p>
+  <p><label>105-108期 </label><font>01</font>,<font>10</font>,18,<font>22</font>,26,<font>28</font>,31<span> (2中2)21组,107期01,10,22,28,😘</span></p>
+  <p><label>103-107期 </label>41,42,43,44,45,<font>46</font><span> (106期46)</span></p>
+  <p><label>105-106期 </label>鸡羊<font>蛇</font>兔猪牛<span> (今年无错，105期蛇)</span></p>
+  <p><label>105-109期 </label>15,16,17,18,19,<font>35</font>,37 <span>(105期35)</span></p>
+  <p><label>103-107期 </label>兔<font>蛇</font>鸡羊 <span>(今年无错，104期蛇)</span></p>
+  <p><label>103-105期 </label>鼠马龙狗鸡<font>蛇</font><span> (今年错1，104期蛇)</span></p>
+  <p><label>103-111期 </label><font>11</font>,22,33,44<span> (今年错1，104期11)</span></p>
   </div>
 
   <!-- 底部透明灰色层 -->
