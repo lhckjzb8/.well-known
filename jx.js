32b86985bcca0f0db4c6f92f42608c1efdd5adc8
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-10-23 08:33:54 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -102,7 +102,7 @@ background-color: rgb(0, 0, 0, 0.03);
 </style>
 <div id="tab-list">
 <div class="minVideoViewBox" id="list">
-<li class="active">极限资料<span class="gg">ok</span></li>
+<li class="active">极限资料<span class="gg">nb</span></li>
 <li>英子九肖<span class="gg">ok</span></li>
 <li>土豆六肖<span class="gg"></span></li>
 <li>天空特围<span class="gg">ok</span></li>
@@ -123,12 +123,12 @@ background-color: rgb(0, 0, 0, 0.03);
 
 <div class="jxzl"><label>111期-120期：</label><div class="gdjx">机会极限② <font>未开</font></div><br>
 11,41 <span>(包肖+尾99.99%稳)</span><div class="gdkj">一年只有几次机会</div><br>
-<span> (20,21,22年各1轮，23年第3轮。无错) </span><div class="gdkj">还有<font>1期</font></div>
+<span> (20,21,22年各1轮,23年第3轮,无错) </span><div class="gdkj">还有<font>1期</font></div>
 </div><!--定位平六06的平一+对冲同尾,十期内开-->
 
 <div class="jxzl"><label>119期-122期：</label><div class="gdjx">机会极限③ <font>未开</font></div><br>
 狗龙<div class="gdkj">一年只有几次机会</div><br>
-<span> (20年1轮,21年3轮,22年1轮，23年第2轮。无错) </span><div class="gdkj">还有<font>3期</font></div>
+<span> (20年1轮,21年3轮,22年1轮,23年第2轮,无错) </span><div class="gdkj">还有<font>3期</font></div>
 </div><!--定位平六49的平一+对冲,四期内开-->
 
 <div class="jxzl"><label>120期-123期：</label><div class="gdjx">固定极限① <font>未开</font></div><br>
