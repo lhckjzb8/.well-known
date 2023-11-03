d0197d04756bda96a281bb3c1266bc6054ec8d36
lhckjzb8 <141644906+lhckjzb8@users.noreply.github.com>
2023-11-03 16:34:22 +0800

    Update jx.js

--- jx.js
+++ jx.js
@@ -450,7 +450,7 @@ if(glistli[this.index].offsetLeft<gglist.offsetWidth*0.1){
 ggdiv.style.marginLeft ="";
 gglist.scrollLeft="";
 }
-if(glistli[this.index].offsetLeft>gglist.offsetWidth*0.95){
+if(glistli[this.index].offsetLeft>gglist.offsetWidth*0.9){
 ggdiv.style.marginLeft =glistli[this.index].offsetLeft+"px";;
 gglist.scrollLeft=glistli[this.index].offsetLeft;
 }
